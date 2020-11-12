754a17a05a6699028ca4d76fbd295bfed6628379
abnerjs <abner.js05@gmail.com>
2020-11-12 20:39:33 -0300

    change to cards and make the font-size taller

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,11 @@
 import { Component } from '@angular/core';
+import { StorageService } from '../service/storage.service';
+
+interface Tarefa {
+  id: number;
+  descricao: string;
+  concluida: boolean;
+}
 
 @Component({
   selector: 'app-home',
@@ -6,7 +13,43 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  public tarefa: Tarefa = {} as any;
+  public tarefas: Tarefa[] = [];
 
-  constructor() {}
+  constructor(private storage: StorageService) {
+    this.storage.recuperar('tarefas').then((data: any) => {
+      if (data != null) {
+        this.tarefas = data;
+      } else {
+        this.tarefas = [];
+      }
+    });
+  }
 
+  adicionar() {
+    this.tarefa.id = new Date().getTime();
+    this.tarefa.concluida = false;
+    this.tarefas.push(this.tarefa);
+    console.log(this.tarefas);
+    this.tarefa = {} as any;
+
+    this.storage.armazenar('tarefas', this.tarefas);
+  }
+
+  finalizar(id: number) {
+    const t = this.tarefas.find((item) => {
+      if (item.id === id) {
+        return item;
+      }
+    });
+
+    t.concluida = !t.concluida;
+    this.storage.armazenar('tarefas', this.tarefas);
+  }
+
+  excluir(id: number) {
+    this.tarefas = this.tarefas.filter((item) => {
+      return item.id !== id;
+    });
+  }
 }
